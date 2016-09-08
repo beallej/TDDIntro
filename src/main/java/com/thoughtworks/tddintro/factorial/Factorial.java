bccf4c61961f068add28e5d6bbab5136af28fd1f
Josie Bealle <josiebealle@gmail.com>
2016-09-08 12:18:24 -0500

    Factorial

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -1,7 +1,16 @@
 package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
-    public Integer compute(int i) {
-        return -1;
+    public Integer compute(int i) throws IllegalArgumentException {
+        if (i < 0) {
+            throw new IllegalArgumentException("Number must be 0 or greater");
+        }
+        else if (i == 0) {
+            return 1;
+        }
+        else {
+            return i * compute(i - 1);
+        }
+
     }
 }
